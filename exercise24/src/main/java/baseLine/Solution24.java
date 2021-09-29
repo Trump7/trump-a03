83266500986c1d93cef8ab5fd4e8fcdddc7c5478
Trump7 <trump@knights.ucf.edu>
2021-09-29 18:26:28 -0400

    Program 24 Pseudocode

---
+++ exercise24/src/main/java/baseLine/Solution24.java
@@ -0,0 +1,39 @@
+/*
+    This program will take in two strings from the user
+    and figure out if they are anagrams.
+ */
+
+
+
+package baseLine;
+
+import java.util.Scanner;
+
+public class Solution24 {
+    //isAnagram returns a true or false given if both strings are anagrams
+    boolean isAnagram(char string1[], char string2[]){
+        //Declare variables for the length of both string's
+        //int str1 = string1.length()
+
+        //Check if both strings are same length
+        // if str1 != str2 return false
+
+        //Sort the strings in alphabetical order
+
+        //loop through each letter and if they all match return true
+        return true;
+    }
+
+    public static void main(String[] args) {
+        //Declare two chars for user input
+        //Declare a string named result
+
+        //Scan user input for string1 and string2
+
+        //call if(isAnagram(str1, str2))
+        // if true set result to "are anagrams"
+        //else set result to "are not anagrams"
+
+        //print both strings and the result
+    }
+}

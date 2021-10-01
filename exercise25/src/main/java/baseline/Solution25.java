8d0e13d75e420b09a615d840049ba749282acffc
Trump7 <trump@knights.ucf.edu>
2021-09-30 23:39:15 -0400

    Solution25 Pseudocode

---
+++ exercise25/src/main/java/baseline/Solution25.java
@@ -0,0 +1,26 @@
+package baseline;
+
+public class Solution25 {
+
+    public int passwordValidator(String password){
+        //declare variable to take in string length
+        //create a char array and set it equal to lowercase password
+        //set up two if statements one for >= 8 characters and one for <.
+        //set up a for loop to go through each character of the string for both if statements
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+        //Going to have to create a scanner
+        //Ask user for their password
+        //Take in password in a String
+        //call passwordValidator function within a switch
+        //case 0 result = a password of unknown strength.
+        //case 1 result = a very weak password.
+        //case 2 result = a weak password.
+        //case 3 result = a strong password.
+        //case 4 result = a very strong password.
+        //A single print statement to print the password and the result
+    }
+}

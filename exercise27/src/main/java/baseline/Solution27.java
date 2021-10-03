61568d67186869f809acab6636be3d079c7f1e4c
Trump7 <trump@knights.ucf.edu>
2021-10-03 01:54:05 -0400

    Exercise27 Solution

--- exercise27/src/main/java/baseline/Solution27.java
+++ exercise27/src/main/java/baseline/Solution27.java
@@ -4,66 +4,132 @@
  */
 package baseline;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Solution27 {
     //create an output string and set it equal to ""
+    static String output = "";
 
-    public void validateFirstName(String firstName){
+    public static boolean validateFirstName(String firstName){
         //create a temp boolean variable
-        //if(firstName.length() < 2)
-        // set temp = false
-        // set output = "The first name must be at least 2 characters long.\n"
-        //if(lastName.length() = 0)
-        // set temp = false
-        // set output = "The last name must be filled in.\n"
+        boolean temp = true;
+        if(firstName.length() < 2){
+            // set temp = false
+            temp = false;
+            // set output = "The first name must be at least 2 characters long.\n"
+            output = output + "The first name must be at least 2 characters long.\n";
+        }
+        if(firstName.length() == 0){
+            // set temp = false
+            temp = false;
+            // set output = "The first name must be filled in.\n"
+            output = output + "The first name must be filled in.\n";
+        }
         //return the temp boolean value
+        return temp;
     }
 
-    public void validateLastName(String lastName){
+    public static boolean validateLastName(String lastName){
         //create a temp boolean variable
-        //if(lastName.length() < 2)
-        // set temp = false
-        // set output = "The last name must be at least 2 characters long.\n"
-        //if(lastName.length() = 0)
-        // set temp = false
-        // set output = "The last name must be filled in.\n"
+        boolean temp = true;
+        if(lastName.length() < 2){
+            // set temp = false
+            temp = false;
+            // set output = "The last name must be at least 2 characters long.\n"
+            output = output + "The last name must be at least 2 characters long.\n";
+        }
+        if(lastName.length() == 0){
+            // set temp = false
+            temp = false;
+            // set output = "The last name must be filled in.\n"
+            output = output + "The last name must be filled in.\n";
+        }
         //return the temp boolean value
+        return temp;
     }
 
-    public void validateZip(String zip){
+    public static boolean validateZip(String zip){
         //create a temp boolean variable
+        boolean temp = true;
         //try to turn String zip into an integer
-        //if that fails it means there is a different character in there
-        // set temp to false
-        // set output = "The zip code must be a 5-digit number.\n"
-        //if that does not fail, zip.length() has to be equal to 5
-        // if not set temp to false and the output to the same as before
+        try{
+            int tempZip = Integer.parseInt(zip);
+            //if that does not fail, zip.length() has to be equal to 5
+            if(zip.length() != 5){
+                // if not set temp to false and the output to the same as before
+                temp = false;
+                output = output + "The zip code must be a 5-digit number.\n";
+            }
+        }catch(Exception e){
+            //if that fails it means there is a different character in there
+            // set temp to false
+            temp = false;
+            // set output = "The zip code must be a 5-digit number.\n"
+            output = output + "The zip code must be a 5-digit number.\n";
+        }
         //return the temp boolean value
+        return temp;
     }
 
-    public void validateID(String id){
+    public static boolean validateID(String id){
         //create a temp boolean variable
+        boolean temp = true;
         // Use regex to make a template for how the id should look
+        //Any Capital or Lowercase 2 letters with 1 hyphen and any four numbers range (0 to 9)
+        String template = "[a-zA-Z]{2}[-][0-9]{4}";
         //if template does not match the string id
-        // set temp = false
-        // set output = "The employee ID must be in the format of AA-1234."
+        if(!Pattern.matches(template, id)){
+            // set temp = false
+            temp = false;
+            // set output = "The employee ID must be in the format of AA-1234."
+            output = output + "The employee ID must be in the format of AA-1234.\n";
+        }
         //return the temp boolean value
+        return temp;
     }
 
-    public void validateInput(String firstName, String lastName, String zip, String id){
+    public static void validateInput(String firstName, String lastName, String zip, String id){
         //make four boolean variables for all parameters
+        boolean isfName;
+        boolean islName;
+        boolean isZip;
+        boolean isID;
         //call each validate function with its corresponding
         // String and set them equal to their boolean variable
+        isfName = validateFirstName(firstName);
+        islName = validateLastName(lastName);
+        isID = validateID(id);
+        isZip = validateZip(zip);
 
-        //if(firstName == true && lastName == true && zip == true && id == true)
-        //Then change output string to "There were no errors found"
+        //if all values are true
+        if(isfName && islName && isZip && isID){
+            //Then change output string to "There were no errors found"
+            output = "There were no errors found";
+        }
         //print output outside the if statement
+        System.out.print(output);
     }
 
     public static void main(String[] args) {
         //Create a scanner for user input
+        Scanner input = new Scanner(System.in);
         //Create four strings to hold all the input data
+        String firstName;
+        String lastName;
+        String zip;
+        String id;
         //Make four print statements, with corresponding scanner in statements
         //asking for first name, last name, zip, and ID
+        System.out.print("Enter the first name: ");
+        firstName = input.nextLine();
+        System.out.print("Enter the last name: ");
+        lastName = input.nextLine();
+        System.out.print("Enter the zip: ");
+        zip = input.nextLine();
+        System.out.print("Enter the id: ");
+        id = input.nextLine();
         //call validateInput with all four parameters
+        validateInput(firstName, lastName, zip, id);
     }
 }
--- exercise27/src/test/java/Solution27Test.java
+++ exercise27/src/test/java/Solution27Test.java
@@ -2,6 +2,7 @@
  *  UCF COP3330 Fall 2021 Assignment 3 Solutions
  *  Copyright 2021 Matt Trump
  */
+import baseline.Solution27;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,25 +10,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Solution27Test {
     //create a constructor for Solution27
+    Solution27 base = new Solution27();
 
     @Test
     void validateFirstNameTest(){
         //validate first name with example first name
-        assertEquals();
+        assertEquals(true, base.validateFirstName("john"));
     }
     @Test
     void validateLastNameTest(){
         //validate last name with example last name
-        assertEquals();
+        assertEquals(true, base.validateLastName("johnson"));
     }
     @Test
     void validateZipTest(){
         //validate zip code with example zip code
-        assertEquals();
+        assertEquals(true, base.validateZip("12345"));
     }
     @Test
     void validateIDTest(){
         //validate ID with example ID
-        assertEquals();
+        assertEquals(true, base.validateID("AB-1234"));
     }
 }

ec5e3da50a24fcb945fad80baca583c5d0f049a5
Trump7 <trump@knights.ucf.edu>
2021-10-03 04:50:16 -0400

    Exercise31 Psuedocode

---
+++ exercise31/src/main/java/Solution31.java
@@ -0,0 +1,47 @@
+public class Solution31 {
+    String table = "";
+
+    public static int getTargetHeartRate(int restingHeartRate, int age, double intensity){
+        //create an int variable for targetHeartRate
+        //targetHeartRate = (((220 − age) − restingHR) × intensity) + restingHR
+        //
+        //return targetHeartRate;
+    }
+
+    public static void generateTable(int restingHeartRate, int age){
+        //Add the header to the table "Intensity | Rate"
+        //add a line separating the header from the table
+        //make a for loop starting from 55 going up to 95 with i + 5
+        //add i % to the left side then
+        //after the pipe (|) call getTargetHeartRate(restingHeartRate, age, i/100.0)
+    }
+
+    public static void main(String[] args) {
+        //Create a scanner
+        //Create two variables: one for resting heart rate and one for age
+        //create a boolean temp set equal to true
+        //create a boolean temp1 set equal to true
+
+        //create a do while loop
+        //Prompt user to enter heart rate
+        //if(input.hasNextInt()
+        //store input in heart rate variable
+        //temp = false
+        //else
+        //temp = true;
+        //end loop
+
+        //create a do while loop
+        //Prompt user to enter age
+        //if(input.hasNextInt()
+        //store input in age variable
+        //temp = false
+        //else
+        //temp = true;
+        //end loop
+
+        //Pass heart rate and age to generateTable
+
+        //Print the table
+    }
+}
---
+++ exercise31/src/test/java/Solution31Test.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Solution31Test {
+
+    @Test
+    void testTargetHeartRate() {
+        //compare a solved target heart rate to the one the program produces
+        //assertEquals();
+    }
+
+    @Test
+    void testGenerateTable() {
+        //compare a pre-made table to the one the program produces
+        //assertEquals();
+    }
+}

52b351a63a3d688536dab0489ff6e90f13aa1928
Trump7 <trump@knights.ucf.edu>
2021-10-03 17:18:23 -0400

    Exercise36 Solution

--- exercise36/src/main/java/baseline/Solution36.java
+++ exercise36/src/main/java/baseline/Solution36.java
@@ -5,65 +5,116 @@
 package baseline;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Solution36 {
     public static double average(ArrayList<Integer> numberList){
         //create a temp variable to store result
+        double temp = 0.0;
         //create a for loop to iterate through the array
-        //add total += numberList[i]
+        for(int num: numberList) {
+            //add total += numberList[i]
+            temp += num;
+        }
         //divide the total by numberList.length() to get the average
+        double average = temp / numberList.size();
         //return the average
+        return average;
     }
 
     public static int max(ArrayList<Integer> numberList){
         //create max variable and assign it the first spot in the array
+        int max = numberList.get(0);
         //create a for loop to iterate through the array
-        //if numberList[i] > max
-        // set max equal to numberList[i]
+        for(int num: numberList) {
+            //if numberList[i] > max
+            if(num > max) {
+                // set max equal to numberList[i]
+                max = num;
+            }
+        }
         //and return max at the end
+        return max;
     }
 
     public static int min(ArrayList<Integer> numberList){
         //create min variable and assign it the first spot in the array
+        int min = numberList.get(0);
         //create a for loop to iterate through the array
-        //if numberList[i] < min
-        // set min equal to numberList[i]
+        for(int num: numberList) {
+            //if numberList[i] < min
+            if(num < min) {
+                // set min equal to numberList[i]
+                min = num;
+            }
+        }
         //and return min at the end
+        return min;
     }
 
     public static double std(ArrayList<Integer> numberList){
         //create a variable for the mean of the list and call the average function
+        double average = average(numberList);
         //create a variable to hold the total
+        double total = 0;
         //use a for loop to go through the whole list
-        //Using part of the standard Deiviation equation inside the for loop
+        for(int num: numberList) {
+            //Using part of the standard deviation equation inside the for loop
+            total = total + Math.pow((num-average), 2);
+        }
         //once outside the loop take the square root of the total
+        double stdDev = Math.sqrt(total/numberList.size());
         //return the total value
+        return stdDev;
     }
+
     public static void printResults(ArrayList<Integer> numberList){
         //print out numbers: followed by all numbers in the array
+        System.out.println("Numbers: " + numberList.toString());
         //print out average (and call average function for result)
+        System.out.println("The average is " + average(numberList));
         //print out min (and call min function for result)
+        System.out.println("The minimum is " + min(numberList));
         //print out max (and call max function for result)
+        System.out.println("The maximum is " + max(numberList));
         //print out std (and call std function for result)
+        System.out.println("The standard deviation is " + String.format("%.2f", std(numberList)));
     }
 
     public static void main(String[] args) {
         //Create a scanner
+        Scanner input = new Scanner(System.in);
         //Create an integer arrayList
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
         //Create a temp string for user input
+        String temp = "";
 
         //create a do while loop
-        //  Check to make sure the number is an int
-        //  if not return invalid input and start from top of loop
-        //  print user to enter number
-        //  store number in temp
-        //
-        //  if the temp string is not equal to "done"
-        //     try to parse the int into another int placeholder
-        //     if that works store it in the arraylist
-        //     if that doesn't work throw an exception that does not add it to the list
-        //end loop with while condition of not temp.equals("done")
-        //
+        do {
+            //  print user to enter number
+            System.out.print("Enter a number: ");
+            //  store number in temp
+            temp = input.next();
+
+            //  if the temp string is not equal to "done"
+            if (!temp.equals("done")) {
+                //     try to parse the int into another int placeholder
+                try {
+                    //  Check to make sure the number is an int
+                    int num = Integer.parseInt(temp);
+                    //     if that works store it in the arraylist
+                    numbers.add(num);
+                } catch (NumberFormatException nfe) {
+                    //  if not return invalid input and start from top of loop
+                    System.out.println("Invalid Input, try again.");
+                }
+
+            }
+            //end loop with while condition of not temp.equals("done")
+        }while(!temp.equals("done"));
+
         // call print results function
+
+        printResults(numbers);
     }
 }
--- exercise36/src/test/java/Solution36Test.java
+++ exercise36/src/test/java/Solution36Test.java
@@ -2,31 +2,67 @@
  *  UCF COP3330 Fall 2021 Assignment 3 Solutions
  *  Copyright 2021 Matt Trump
  */
+import baseline.Solution36;
 import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 
 public class Solution36Test {
+    Solution36 solution = new Solution36();
+    ArrayList<Integer> numbers = new ArrayList<Integer>();
+
 
     @Test
     public void stdTest(){
         //Do the calculations by hand and store it in a double
+        double actual = 353.55;
+
+        numbers.add(100);
+        numbers.add(200);
+        numbers.add(1000);
+        numbers.add(300);
         //Compare the hand done calculation with the functions calculation
+        assertEquals(actual, solution.std(numbers), 0.01);
     }
 
     @Test
     public void minTest(){
         //Do the calculations by hand and store it in a int
+        int actual = 100;
+
+        numbers.add(100);
+        numbers.add(200);
+        numbers.add(1000);
+        numbers.add(300);
         //Compare the hand done calculation with the functions calculation
+        assertEquals(actual, solution.min(numbers));
     }
 
     @Test
     public void maxTest(){
         //Do the calculations by hand and store it in a int
+        int actual = 1000;
+
+        numbers.add(100);
+        numbers.add(200);
+        numbers.add(1000);
+        numbers.add(300);
         //Compare the hand done calculation with the functions calculation
+        assertEquals(actual, solution.max(numbers));
     }
 
     @Test
     public void averageTest(){
         //Do the calculations by hand and store it in a double
+        double actual = 400;
+
+        numbers.add(100);
+        numbers.add(200);
+        numbers.add(1000);
+        numbers.add(300);
         //Compare the hand done calculation with the functions calculation
+        assertEquals(actual, solution.average(numbers));
     }
 }

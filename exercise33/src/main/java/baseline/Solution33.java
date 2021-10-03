481c60fd99074a33d9ff213efc500a188c6e9784
Trump7 <trump@knights.ucf.edu>
2021-10-03 09:38:26 -0400

    Exercise33 Pseudocode

---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,11 @@
+package baseline;
+
+public class Solution33 {
+    public static void main(String[] args) {
+        //create an array with "Yes," "No," "Maybe," or "Ask again later."
+        //create a scanner for user input
+        //ask the user what their question is
+        //take in user input to a temp variable
+        //Select a random number between 0 and 3 and print said statement from the array
+    }
+}
---
+++ exercise33/src/test/java/Solution33Test.java
@@ -0,0 +1,5 @@
+public class Solution33Test {
+    //Since the prompt says to take in user input
+    //and do nothing with it. but to return a random string from an array
+    //there is really no unit testing to be done.
+}

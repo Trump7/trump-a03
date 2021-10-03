6a0755ffa9fdafa2fcabada3f671ae15e86d60f9
Trump7 <trump@knights.ucf.edu>
2021-10-03 11:40:50 -0400

    Exercise34 Solution

--- exercise34/src/main/java/baseline/Solution34.java
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -4,33 +4,54 @@
  */
 package baseline;
 
+import java.util.Scanner;
+
 public class Solution34 {
     public static void printArray(String[] employee){
         //print "There are" + employee.length() + " employees:"
+        System.out.println("\nThere are " + employee.length + " employees:");
         //create a for loop that is the length of employee array
-        //print each of the items in the array
+        for(int i = 0; i < employee.length; i++){
+            //print each of the items in the array
+            System.out.println(employee[i]);
+        }
     }
 
-    public static String[] removeFromArray(){
+    public static String[] removeFromArray(String[] employee){
+        Scanner input = new Scanner(System.in);
         //create a string variable to hold the removal name
+        String removal = "";
         //Create a temp String array with length - 1
+        String[] temp = new String[employee.length - 1];
         //print Enter an employee name to remove
+        System.out.print("\nEnter an employee name to remove: ");
         //set the string variable equal to the users input
+        removal = input.nextLine();
         //make a for loop (with i and a) no longer than the array length
-        //  if !array[i].equals(string variable)
-        //  temp[a++] = employee[i]
+        for(int i = 0, a = 0; i < employee.length; i++){
+            //  if !array[i].equals(string variable)
+            if(!employee[i].equals(removal)){
+                //  temp[a++] = employee[i]
+                temp[a++] = employee[i];
+            }
+        }
         //return temp array
+        return temp;
     }
 
     public static void main(String[] args) {
         //initialize a String array with employee names:
         //John Smith, Jackie Jackson, Chris Jones, Amanda Cullen, Jeremy Goodwin
+        String[] employee = new String[] {"John Smith", "Jackie Jackson", "Chris Jones", "Amanda Cullen", "Jeremy Goodwin"};
 
         //call print array function with the employee string array as a parameter
+        printArray(employee);
 
         //call removeFromArray function to prompt the user to delete an employee
         //Set removeFromArray equal to the original array
+        employee = removeFromArray(employee);
 
         //call print array function again to show the new employee number and the current employees
+        printArray(employee);
     }
 }
--- exercise34/src/test/java/Solution24Test.java
+++ exercise34/src/test/java/Solution24Test.java
@@ -2,16 +2,43 @@
  *  UCF COP3330 Fall 2021 Assignment 3 Solutions
  *  Copyright 2021 Matt Trump
  */
+import baseline.Solution34;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Solution24Test {
+    Solution34 test = new Solution34();
 
     @Test
-    void removeFromArrayTest(){
+    void removeFromArrayTest1(){
         //Create a test array to pass to the removeFromArray
+        String[] example = new String[] {"Matt Trump", "Donald Trump", "Barron Trump"};
         //Also create an answer array to put in assertEqual
-        //assertEquals();
+        String[] answer = new String[] {"Donald Trump", "Barron Trump"};
+
+        String input = "Matt Trump\n";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertArrayEquals(answer, test.removeFromArray(example));
+    }
+
+    @Test
+    void removeFromArrayTest2(){
+        //Create a test array to pass to the removeFromArray
+        String[] example = new String[] {"George Washington", "John Adams", "Barack Obama", "George Bush", "Joe Biden"};
+        //Also create an answer array to put in assertEqual
+        String[] answer = new String[] {"George Washington", "John Adams", "Barack Obama", "George Bush"};
+
+        String input = "Joe Biden\n";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertArrayEquals(answer, test.removeFromArray(example));
     }
 }

ce756320dc914224d2373d5523829857782cfaa8
Trump7 <trump@knights.ucf.edu>
2021-10-03 08:13:29 -0400

    Exercise32 Pseudocode

---
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -0,0 +1,49 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Matt Trump
+ */
+package baseline;
+
+public class Solution32 {
+    public static int randomNumber(int difficulty){
+        //make an if statement for if difficulty = 1
+        //return a random number between 1 and 10
+        //make an if statement for if difficulty = 2
+        //return a random number between 1 and 100
+        //make an if statement for if difficulty = 3
+        //return a random number between 1 and 1000
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        //Create a variable named difficulty
+        //Create a variable named guess
+        //Create a variable named counter
+        //Create a variable named random
+        //Create a scanner for user input
+
+        //Print the title of the game to the screen
+        //Create a while loop
+        //Ask the user what level of difficulty they want
+        //set difficulty equal to that number only if it is an integer
+        //call random = randomNumber(difficulty)
+        //Print out "I have my number. What's your guess?: "
+        //create an if statement if input.hasnextint
+        //  if true, set guess equal to input.nextInt
+        //  add one to counter
+        //  if guess = random
+        //      print out "you got it in" + counter + "guesses"
+        //  if guess < random
+        //      print out "too low, guess again"
+        //  if guess > random
+        //      print out "too high, guess again"
+
+        //outside of the while loop
+        // ask the user if they want to play again.
+        //  if no break and end the program
+
+
+
+    }
+}
---
+++ exercise32/src/test/java/Solution32Test.java
@@ -0,0 +1,6 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Matt Trump
+ */
+public class Solution32Test {
+}

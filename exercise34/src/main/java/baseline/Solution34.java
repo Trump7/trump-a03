3bbd61be65ad346b844df891639260728f95a5b1
Trump7 <trump@knights.ucf.edu>
2021-10-03 11:16:41 -0400

    Exercise24 Pseudocode

---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,36 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Matt Trump
+ */
+package baseline;
+
+public class Solution34 {
+    public static void printArray(String[] employee){
+        //print "There are" + employee.length() + " employees:"
+        //create a for loop that is the length of employee array
+        //print each of the items in the array
+    }
+
+    public static String[] removeFromArray(){
+        //create a string variable to hold the removal name
+        //Create a temp String array with length - 1
+        //print Enter an employee name to remove
+        //set the string variable equal to the users input
+        //make a for loop (with i and a) no longer than the array length
+        //  if !array[i].equals(string variable)
+        //  temp[a++] = employee[i]
+        //return temp array
+    }
+
+    public static void main(String[] args) {
+        //initialize a String array with employee names:
+        //John Smith, Jackie Jackson, Chris Jones, Amanda Cullen, Jeremy Goodwin
+
+        //call print array function with the employee string array as a parameter
+
+        //call removeFromArray function to prompt the user to delete an employee
+        //Set removeFromArray equal to the original array
+
+        //call print array function again to show the new employee number and the current employees
+    }
+}
---
+++ exercise34/src/test/java/Solution24Test.java
@@ -0,0 +1,17 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Matt Trump
+ */
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Solution24Test {
+
+    @Test
+    void removeFromArrayTest(){
+        //Create a test array to pass to the removeFromArray
+        //Also create an answer array to put in assertEqual
+        //assertEquals();
+    }
+}

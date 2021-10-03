7d4e343222270852961cb743cf63773cbfb31e61
Trump7 <trump@knights.ucf.edu>
2021-10-03 19:44:26 -0400

    Exercise38 Solution

--- exercise38/src/main/java/baseline/Solution38.java
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -1,26 +1,56 @@
 package baseline;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Solution38 {
     public static Integer[] filterEvenNumbers(String numList[]){
+        int temp;
         //Create a placeholder Array list
+        ArrayList<Integer> placeholder = new ArrayList<>();
         //Create a for loop to look through every character in numList
-        //convert each instance of the string to an int
-        //Create an if statement to determine if the number is even (num % 2)
-        //  if true add it to the placeholder array list
+        for(int i = 0; i < numList.length; i++){
+            //convert each instance of the string to an int
+            temp = Integer.parseInt(numList[i]);
 
+            //Create an if statement to determine if the number is even (num % 2)
+            if( temp % 2 == 0){
+                //  if true add it to the placeholder array list
+                placeholder.add(temp);
+            }
+        }
         //convert the array list to an int array and return it
+        Integer[] returnList = new Integer[placeholder.size()];
+        returnList = placeholder.toArray(returnList);
+
+        return returnList;
     }
 
     public static void main(String[] args) {
         //create a scanner to take user input
+        Scanner input = new Scanner(System.in);
         //Create a temp String to hold the user list
+        String temp = "";
         //print statement asking the user to enter a list of numbers with spaces in between
+        System.out.print("Enter a list of numbers, separated by spaces: ");
         //assign the user input to temp string
+        temp = input.nextLine();
         //convert the temp string to a list array
+        String unfiltered[] = temp.split(" ");
         //create a new array named evenList and set it equal to filterEvenNumbers(list)
+        Integer filteredList[] = filterEvenNumbers(unfiltered);
 
         //Print out The even numbers are
+        System.out.print("The even numbers are: ");
         //create a for loop to print each number with a space to the user
-        //create an if statement for the last number of the array to place a period
+        for(int i = 0; i < filteredList.length; i++){
+            //create an if statement for the last number of the array to place a period
+            if(i < filteredList.length - 1){
+                System.out.print(filteredList[i] + " ");
+            }
+            else{
+                System.out.print(filteredList[i] + ".");
+            }
+        }
     }
 }
--- exercise38/src/test/java/Solution38Test.java
+++ exercise38/src/test/java/Solution38Test.java
@@ -1,11 +1,29 @@
+import baseline.Solution38;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Solution38Test {
+    Solution38 solution = new Solution38();
 
     @Test
-    public void filterEvenNumbersTest(){
+    public void filterEvenNumbersTest1(){
         //create a list array with unfiltered ints
+        String unfiltered[] = {"1","2","3","4","5","6","7","8"};
         //create an int array with filtered ints
+        Integer[] filtered = {2,4,6,8};
         //compare the filtered ints to filterEvenNumbers(unfiltered)
+        assertArrayEquals(filtered, solution.filterEvenNumbers(unfiltered));
+    }
+
+    @Test
+    public void filterEvenNumbersTest2(){
+        //create a list array with unfiltered ints
+        String unfiltered[] = {"5","10","15","20","25","30","35","40"};
+        //create an int array with filtered ints
+        Integer[] filtered = {10,20,30,40};
+        //compare the filtered ints to filterEvenNumbers(unfiltered)
+        assertArrayEquals(filtered, solution.filterEvenNumbers(unfiltered));
     }
 }

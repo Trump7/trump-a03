06cf2ff7c96f834ddb2ad2fda7585a80016d3b81
Trump7 <trump@knights.ucf.edu>
2021-10-03 12:39:07 -0400

    Exercise35 Pseudocode

---
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -0,0 +1,32 @@
+package baseline;
+
+import java.util.ArrayList;
+
+public class Solution35 {
+    public static ArrayList<String> getNames(){
+        //Create a scanner for user input
+        //Create a String temp variable
+        //Create an arrayList to hold all the names
+        //Make a do while loop
+        //  print Enter a name:
+        //  set user input equal to temp variable
+        // if the temp variable is not blank
+        //      arrayList.add(temp)
+        //end while loop with condition "!temp.isBlank()
+        //return arraylist
+    }
+    public static int generateNum(){
+        //Create a random constructor
+        //Create a placeholder int
+        //set placeholder equal to the random spot in the array
+        //return placeholder int
+    }
+
+    public static void main(String[] args) {
+        //Create a new array list
+        //Create an int named randoNum
+        //call getNames and set it equal to the new array list
+        //call generateNum and set it equal to randoNum
+        //Print the random name is the list of names
+    }
+}
---
+++ exercise35/src/test/java/Solution35Test.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class Solution35Test {
+
+    @Test
+    void getNamesTest(){
+        //create an example arraylist and compare the example one to the user inputted one
+    }
+}

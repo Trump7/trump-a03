d0a20678b8889de8ea49339aa9910e1c3da5a83f
Trump7 <trump@knights.ucf.edu>
2021-10-03 18:12:45 -0400

    Exercise37 Pseudocode

---
+++ exercise37/src/main/java/baseline/Solution37.java
@@ -0,0 +1,46 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Matt Trump
+ */
+package baseline;
+
+public class Solution37 {
+    public static String generatePassword(int minLength, int special, int nums){
+        //Initialize Random()
+        //Initialize a string for password
+        //Initialize the list for special characters
+        //Initialize the list for numbers
+        //Initialize the list for letters
+
+        //Add a for loop to add random letters
+        //Conditions must be < length - special - numbers
+
+        //Add a for loop to add random special characters
+        //Condition must be < special
+
+        //Add a for loop to add random numbers
+        //Condition must be < numbers
+
+        //Add a nested for loop that mixes all the numbers, letters, and special characters around
+        //return String password
+    }
+
+    public static void main(String[] args) {
+        //Create a scanner
+        //Create variable int special
+        //Create variable int length
+        //Create variable int numbers
+
+        //Ask user for the minimum length
+        //Set that value equal to length
+        //Ask user for the # of special characters
+        //Set that value equal to special
+        //Ask user for the # of numbers
+        //Set that value equal to numbers
+
+        //Set String password equal to generatePassword(length, special, numbers)
+
+        //Print the randomly generated password
+
+    }
+}
---
+++ exercise37/src/test/java/Solution37Test.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Matt Trump
+ */
+public class Solution37Test {
+
+    @Test
+    void passwordGeneratorTest1(){
+        // Test to make sure the passwords length >= min length
+    }
+    @Test
+    void passwordGeneratorTest2(){
+        // Test to make sure the passwords letters >= special + numbers
+    }
+}

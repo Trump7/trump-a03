150f8e5561271f05d4ef5eff479b0d055a7f0b13
Trump7 <trump@knights.ucf.edu>
2021-10-03 04:14:36 -0400

    Exercise30 Pseudocode

---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,22 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Matt Trump
+ */
+package baseline;
+
+public class Solution30 {
+    static String table = "";
+
+    public static void makeTable() {
+        //initialize two variables i and a for the for loops
+        //create a for loop (for i) that starts at one and is <= 12
+        //  within that for loop make another (for a) that starts at one and is <= 12
+        //  also add a character (i * a) to the table string
+    }
+
+
+    public static void main(String[] args) {
+        //this command prints out the multiplication table
+        System.out.println(table);
+    }
+}
---
+++ exercise30/src/test/java/Solution30Test.java
@@ -0,0 +1,17 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Matt Trump
+ */
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class Solution30Test {
+    @Test
+    public void checkMultiplicationTable(){
+        //create a variable to store the multiplication table
+        //compare created string with the one from Solution30
+        //assertEquals();
+    }
+}

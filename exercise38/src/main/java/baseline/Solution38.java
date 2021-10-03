88cffc95d4cf664e181912a199ea7e1d96d79d93
Trump7 <trump@knights.ucf.edu>
2021-10-03 19:22:28 -0400

    Exercise38 Pseudocode

---
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -0,0 +1,26 @@
+package baseline;
+
+public class Solution38 {
+    public static Integer[] filterEvenNumbers(String numList[]){
+        //Create a placeholder Array list
+        //Create a for loop to look through every character in numList
+        //convert each instance of the string to an int
+        //Create an if statement to determine if the number is even (num % 2)
+        //  if true add it to the placeholder array list
+
+        //convert the array list to an int array and return it
+    }
+
+    public static void main(String[] args) {
+        //create a scanner to take user input
+        //Create a temp String to hold the user list
+        //print statement asking the user to enter a list of numbers with spaces in between
+        //assign the user input to temp string
+        //convert the temp string to a list array
+        //create a new array named evenList and set it equal to filterEvenNumbers(list)
+
+        //Print out The even numbers are
+        //create a for loop to print each number with a space to the user
+        //create an if statement for the last number of the array to place a period
+    }
+}
---
+++ exercise38/src/test/java/Solution38Test.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+public class Solution38Test {
+
+    @Test
+    public void filterEvenNumbersTest(){
+        //create a list array with unfiltered ints
+        //create an int array with filtered ints
+        //compare the filtered ints to filterEvenNumbers(unfiltered)
+    }
+}

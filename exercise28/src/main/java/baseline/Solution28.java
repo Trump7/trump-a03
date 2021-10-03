d341510dea0d91999b39f355faa34549e31f2a14
Trump7 <trump@knights.ucf.edu>
2021-10-03 02:42:15 -0400

    Exercise28 Pseudocode

---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,20 @@
+package baseline;
+
+public class Solution28 {
+
+    public static void main(String[] args) {
+        //Create a scanner input
+        //Create a placeholder variable and initialize it to 0
+
+        //Make a while loop while(i != 5)
+        // print enter a number:
+        // make an if else statement and if input.hasnextInt() is true
+        //  placeholder += input.nextInt()
+        //  add one to i as well
+        // else
+        // print out "Integers only please"
+
+        //after the loop is over
+        //print the placeholder as total
+    }
+}
---
+++ exercise28/src/test/java/Solution28Test.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Solution28Test {
+
+    @Test
+    void addingTest(){
+        //assertEquals();
+    }
+}

5b3fdbfff9d53e8c32803a8940f74bd54d63f742
Trump7 <trump@knights.ucf.edu>
2021-10-03 05:29:08 -0400

    Exercise31 Solution

---
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -0,0 +1,94 @@
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution31 {
+    public static String table = "";
+
+    public static int getTargetHeartRate(int restingHeartRate, int age, int intensity){
+        //create an int variable for targetHeartRate
+        double targetHeartRate = 0;
+        //targetHeartRate = (((220 − age) − restingHR) × intensity) + restingHR
+        targetHeartRate = (((220 - age) - restingHeartRate) * (intensity/100.0)) + restingHeartRate;
+        //rounding the target heart rate because it is currently a double
+        return (int)Math.round(targetHeartRate);
+    }
+
+    public static void generateTable(int restingHeartRate, int age){
+        //Add their resting rate and age to the top
+        table = table + String.format("Resting pulse: %d\t\tAge: %d\n\n", restingHeartRate, age);
+        //Add the header to the table "Intensity | Rate"
+        table = table + "Intensity\t| Rate\n";
+        //add a line separating the header from the table
+        table = table + "------------|----------\n";
+        //make a for loop starting from 55 going up to 95 with i + 5
+        for(int i = 55; i <= 95; i += 5){
+            //add i % to the left side then
+            //after the pipe (|) call getTargetHeartRate(restingHeartRate, age, i/100.0)
+            table = table + String.format("%d%%\t\t\t| %2d bpm\n", i, getTargetHeartRate(restingHeartRate,age, i));
+        }
+    }
+
+    public static void main(String[] args) {
+        //Create a scanner
+        Scanner input = new Scanner(System.in);
+        //Create two variables: one for resting heart rate and one for age
+        int restingHeartRate = 0;
+        int age = 0;
+        //create a boolean temp set equal to true
+        boolean temp = true;
+        //create a boolean temp1 set equal to true
+        boolean temp1 = true;
+
+        //create a do while loop
+        do{
+            //Prompt user to enter heart rate
+            System.out.print("What is your resting heart rate? ");
+            //if(input.hasNextInt()
+            if(input.hasNextInt()){
+                //store input in heart rate variable
+                restingHeartRate = input.nextInt();
+                if(restingHeartRate > 0){
+                //temp = false
+                temp = false;
+                }
+                else{
+                    System.out.println("Please enter a valid heart rate");
+                    input.nextLine();
+                }
+            }
+            else{
+                System.out.println("Please enter a valid heart rate");
+                input.nextLine();
+            }
+        }while(temp);
+        //create a do while loop
+        do{
+            //Prompt user to enter age
+            System.out.print("What is your age? ");
+            //if(input.hasNextInt()
+            if(input.hasNextInt()){
+                //store input in age variable
+                age = input.nextInt();
+                if(age > 0){
+                    //temp = false
+                    temp1 = false;
+                }
+                else{
+                    System.out.println("Please enter a valid heart rate");
+                    input.nextLine();
+                }
+            }
+            else{
+                System.out.println("Please enter a valid heart rate");
+                input.nextLine();
+            }
+        }while(temp1);
+
+        //Pass heart rate and age to generateTable
+        generateTable(restingHeartRate, age);
+
+        //Print the table
+        System.out.println(table);
+    }
+}
--- exercise31/src/test/java/Solution31Test.java
+++ exercise31/src/test/java/Solution31Test.java
@@ -1,18 +1,38 @@
+import baseline.Solution31;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Solution31Test {
+    Solution31 test = new Solution31();
 
     @Test
     void testTargetHeartRate() {
+        int realTarget = 165;
+
         //compare a solved target heart rate to the one the program produces
-        //assertEquals();
+        assertEquals(realTarget, test.getTargetHeartRate(65, 22, 75));
     }
 
     @Test
     void testGenerateTable() {
+        String realTable = "Resting pulse: 65\t\tAge: 22\n" +
+                "\n" +
+                "Intensity\t| Rate\n" +
+                "------------|----------\n" +
+                "55%\t\t\t| 138 bpm\n" +
+                "60%\t\t\t| 145 bpm\n" +
+                "65%\t\t\t| 151 bpm\n" +
+                "70%\t\t\t| 158 bpm\n" +
+                "75%\t\t\t| 165 bpm\n" +
+                "80%\t\t\t| 171 bpm\n" +
+                "85%\t\t\t| 178 bpm\n" +
+                "90%\t\t\t| 185 bpm\n" +
+                "95%\t\t\t| 191 bpm\n";
+
+        //generating the table to test
+        test.generateTable(65, 22);
         //compare a pre-made table to the one the program produces
-        //assertEquals();
+        assertEquals(realTable, test.table);
     }
 }

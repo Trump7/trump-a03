b75d57d84970994af1433af09d700940e6431e0e
Trump7 <trump@knights.ucf.edu>
2021-10-03 15:55:28 -0400

    Exercise35 Solution

--- exercise35/src/main/java/baseline/Solution35.java
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -1,32 +1,58 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Matt Trump
+ */
 package baseline;
 
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Solution35 {
+
     public static ArrayList<String> getNames(){
         //Create a scanner for user input
+        Scanner input = new Scanner(System.in);
         //Create a String temp variable
+        String temp;
         //Create an arrayList to hold all the names
+        ArrayList<String> tempNames = new ArrayList<String>();
         //Make a do while loop
-        //  print Enter a name:
-        //  set user input equal to temp variable
-        // if the temp variable is not blank
-        //      arrayList.add(temp)
-        //end while loop with condition "!temp.isBlank()
+        do{
+            //  print Enter a name:
+            System.out.print("Enter a name: ");
+            //  set user input equal to temp variable
+            temp = input.nextLine();
+
+            // if the temp variable is not blank
+            if(!temp.isBlank() && !temp.isEmpty()){
+                tempNames.add(temp);
+            }
+        }while(!temp.isBlank() && !temp.isEmpty());
+
         //return arraylist
+        return tempNames;
     }
-    public static int generateNum(){
+    public static int generateNum(ArrayList<String> list){
         //Create a random constructor
+        Random random = new Random();
         //Create a placeholder int
+        int placeholder = random.nextInt(list.size());
         //set placeholder equal to the random spot in the array
         //return placeholder int
+        return placeholder;
     }
 
     public static void main(String[] args) {
         //Create a new array list
+        ArrayList<String> names = new ArrayList<String>();
         //Create an int named randoNum
+        int randoNum = 0;
         //call getNames and set it equal to the new array list
+        names = getNames();
         //call generateNum and set it equal to randoNum
+        randoNum = generateNum(names);
         //Print the random name is the list of names
+        System.out.println("The winner is ... " + names.get(randoNum) + ".");
     }
 }
--- exercise35/src/test/java/Solution35Test.java
+++ exercise35/src/test/java/Solution35Test.java
@@ -1,9 +1,36 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Matt Trump
+ */
+import baseline.Solution35;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Solution35Test {
+    Solution35 test = new Solution35();
 
     @Test
     void getNamesTest(){
         //create an example arraylist and compare the example one to the user inputted one
+        List<String> example = new ArrayList<String>();
+        List<String> tester = new ArrayList<String>();
+        example.add("Matt");
+        example.add("Phat");
+        example.add("Whack");
+
+        String input = "Matt\nPhat\nWhack\n";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        tester = test.getNames();
+
+        assertEquals(example, tester);
     }
 }

4b34a338642bdbd74e04fb250b82dd4391a5899d
Trump7 <trump@knights.ucf.edu>
2021-10-03 04:26:57 -0400

    Exercise30 Solution

--- exercise30/src/main/java/baseline/Solution30.java
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -5,17 +5,26 @@
 package baseline;
 
 public class Solution30 {
-    static String table = "";
+    public static String table = "";
 
     public static void makeTable() {
         //initialize two variables i and a for the for loops
+        int i;
+        int a;
         //create a for loop (for i) that starts at one and is <= 12
-        //  within that for loop make another (for a) that starts at one and is <= 12
-        //  also add a character (i * a) to the table string
+        for(i = 1; i <= 12; i++){
+            //  within that for loop make another (for a) that starts at one and is <= 12
+            for(a = 1; a <= 12; a++){
+                //  also add a character (i * a) to the table string
+                table = table + String.format("%4d", i * a);
+            }
+            table = table + "\n";
+        }
     }
 
-
     public static void main(String[] args) {
+        //Call makeTable to add the necessary digits to String table
+        makeTable();
         //this command prints out the multiplication table
         System.out.println(table);
     }

726f4f8cb7ee91a868870fc9cd96d222568a8874
Trump7 <trump@knights.ucf.edu>
2021-10-03 03:35:17 -0400

    Exercise29 Pseudocode

---
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,32 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Matt Trump
+ */
+package baseline;
+
+public class Solution29 {
+    public static int getRate(){
+        //Create a scanner
+        //create a variable for rate and set it equal to 0
+        //create a boolean variable temp equal to true
+
+        //create a do while loop with the parameter temp = true;
+        //print the question, What is the rate of return?
+        //if(input.hasNextInt())
+        //set input equal to rate
+        //another if statement to make sure it is greater than zero
+        //else print out "Sorry. That's not a valid input."
+        //else{
+        // print out "Sorry. That's not a valid input."
+        //and go to the next line to repeat the loop
+        //until a valid answer is entered
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        //create a variable for years and set it equal to 0
+        //create a variable for rate and set it equal to getRate()
+        //calculate years = 72 / rate
+        //print "It will take " + years + " years to double your initial investment."
+    }
+}
---
+++ exercise29/src/test/java/Solution29Test.java
@@ -0,0 +1,9 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Matt Trump
+ */
+import org.junit.jupiter.api.Test;
+
+public class Solution29Test {
+
+}
